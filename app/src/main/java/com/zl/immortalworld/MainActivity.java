41376c46a808c68c090c99b102dab97f8af30993
zl <764958658@qq.com>
2017-09-08 19:36:01 +0800

    新建项目

---
+++ app/src/main/java/com/zl/immortalworld/MainActivity.java
@@ -0,0 +1,17 @@
+package com.zl.immortalworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * 仙界
+ * @author zl
+ * @date 2017/9/8 16:22
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
